21f64d0d599b5fe21105eee496b31c3ef0c047ab
James Lane <likeamojay@users.noreply.github.com>
2025-12-10 17:52:25 -0700

    Initial Commit

---
+++ TabletCommandExercise/TabletCommandExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabletCommandExerciseApp.swift
+//  TabletCommandExercise
+//
+//  Created by James on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TabletCommandExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
